d82aea94896d201563377fccfe12d3499e77de16
Moondoyeon <mmmdo21@gmail.com>
2023-09-03 00:42:57 +0900

    style(font): 레트로틱 폰트 적용

--- src/libs/style/reset.ts
+++ src/libs/style/reset.ts
@@ -8,8 +8,35 @@ const reset = css`
     margin: 0;
     padding: 0;
   }
+  @font-face {
+    font-family: Galmuri11;
+    font-style: normal;
+    font-stretch: normal;
+    font-weight: 400;
+    font-display: swap;
+    src:
+      local('Galmuri11 Regular'),
+      url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/Galmuri11.woff2')
+        format('woff2'),
+      url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/Galmuri11.ttf')
+        format('truetype');
+  }
+
+  @font-face {
+    font-family: Galmuri11;
+    font-style: normal;
+    font-stretch: normal;
+    font-weight: 700;
+    font-display: swap;
+    src:
+      local('Galmuri11 Bold'),
+      url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/Galmuri11-Bold.woff2')
+        format('woff2'),
+      url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/Galmuri11-Bold.ttf')
+        format('truetype');
+  }
   body {
-    font-family: 'Noto Sans KR', sans-serif;
+    font-family: Galmuri11, 'Noto Sans KR', sans-serif;
     min-width: 320px; // 해당 너비 이하일때, 디자인이 깨지는 것 방지
   }
   button {
