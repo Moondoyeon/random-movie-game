f6b2ec0f19b151f0a0904705f8cd2f71a7e71376
Moondoyeon <mmmdo21@gmail.com>
2023-09-04 11:09:01 +0900

    fix(webFont): woff2가 지원되지 않는 브라우저를 위해 woff 추가

--- src/libs/style/reset.ts
+++ src/libs/style/reset.ts
@@ -18,6 +18,8 @@ const reset = css`
       local('Galmuri11 Regular'),
       url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/Galmuri11.woff2')
         format('woff2'),
+      url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/Galmuri11.woff')
+        format('woff'),
       url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/Galmuri11.ttf')
         format('truetype');
   }
@@ -32,6 +34,8 @@ const reset = css`
       local('Galmuri11 Bold'),
       url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/Galmuri11-Bold.woff2')
         format('woff2'),
+      url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/Galmuri11-Bold.woff')
+        format('woff'),
       url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/Galmuri11-Bold.ttf')
         format('truetype');
   }
