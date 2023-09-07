cb56002e0037fb2cecdc408aae40020e13602016
Moondoyeon <mmmdo21@gmail.com>
2023-09-07 17:36:37 +0900

    fix(env): jest가 env 값 인식하게 함

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,7 @@
 import { server } from 'mock/server';
+import dotenv from 'dotenv';
 
+dotenv.config();
 beforeAll(() => server.listen({ onUnhandledRequest: 'bypass' }));
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
