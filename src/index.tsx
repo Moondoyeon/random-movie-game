8a7b1fe8db0ce9abf12f0124a7c5e90ce76af8c6
Moondoyeon <mmmdo21@gmail.com>
2023-10-09 17:01:12 +0900

    chore(src/index.tsx): Router, Suspense 컴포넌트 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import { Global } from '@emotion/react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
 import App from './App';
 import reset from 'libs/style/reset';
 import reportWebVitals from './reportWebVitals';
 import RootErrorFallback from 'libs/components/@helper/ErrorBoundary/RootErrorFallback';
 import ModalProvider from 'libs/context/ModalContext';
-
+import { Suspense } from 'react';
+import Loading from 'libs/components/@common/Loading';
 // import { worker } from 'mock/browser';
 
 // if (process.env.NODE_ENV === 'development') {
@@ -21,14 +22,18 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <BrowserRouter>
+  <>
     <Global styles={reset} />
-    <ModalProvider>
-      <ErrorBoundary FallbackComponent={RootErrorFallback}>
-        <App />
-      </ErrorBoundary>
-    </ModalProvider>
-  </BrowserRouter>,
+    <Router>
+      <ModalProvider>
+        <Suspense fallback={<Loading whiteBoard={false} />}>
+          <ErrorBoundary FallbackComponent={RootErrorFallback}>
+            <App />
+          </ErrorBoundary>
+        </Suspense>
+      </ModalProvider>
+    </Router>
+  </>,
 );
 
 // If you want to start measuring performance in your app, pass a function
