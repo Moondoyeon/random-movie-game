68e19b321b759880e68e4081d97bea4dcce55b6a
Moondoyeon <mmmdo21@gmail.com>
2023-10-09 16:57:25 +0900

    reafactor(API request): test 환경에서 사용하는 함수를 따로 만듦

--- src/libs/hooks/useMovieData.ts
+++ src/libs/hooks/useMovieData.ts
@@ -3,6 +3,7 @@ import { httpForTest } from 'libs/api/http';
 import { MovieData } from 'libs/types/game';
 import { initNum } from 'libs/utils';
 import { promiseWrapper } from 'libs/utils/promiseWrapper';
+
 import { useEffect, useState } from 'react';
 import { useErrorBoundary } from 'react-error-boundary';
 
@@ -17,30 +18,27 @@ function useMovieData({ selected }: { selected: Record<string, string> }) {
     const date = `${selected.year}${month}${day}`;
     return date;
   };
-
-  const getMovieList = async () => {
+  const getTestMovieList = async () => {
+    // yarn test (for Jest in Node)
     try {
-      if (
-        // yarn start && browser msw 실행 및 실제 네트워크 요청하는 개발환경 에서 사용되는 로직
-        // browser msw 실행해도 되는 이유) msw는 어차피 요청 url 만 가로채서 요청핸들러에 정의해놓은 url과
-        // 동일하기만 하면 mocked Response를 응답해주기 때문
-        process.env.NODE_ENV === 'development' ||
-        process.env.NODE_ENV === 'production'
-      ) {
-        return await CacheApi.getMovieData({
-          targetDt: formatDate(),
-          multiMovieYn: selected.type,
-          repNationCd: selected.country,
-        });
-      }
-      if (process.env.NODE_ENV === 'test') {
-        // yarn test 에서 사용되는 요청 로직 for Jest in Node
-        return await httpForTest.get('', {
-          targetDt: formatDate(),
-          multiMovieYn: selected.type,
-          repNationCd: selected.country,
-        });
-      }
+      return await httpForTest.get('', {
+        targetDt: formatDate(),
+        multiMovieYn: selected.type,
+        repNationCd: selected.country,
+      });
+    } catch (error) {
+      showBoundary(error);
+    }
+  };
+  const getMovieList = async () => {
+    // yarn start && browser msw (dev, produvtion)
+    // browser msw) msw는 요청 url을 가로채서 요청 핸들러에 정의해놓은 url과 동일하기만 하면 mocked Response를 응답
+    try {
+      return await CacheApi.getMovieData({
+        targetDt: formatDate(),
+        multiMovieYn: selected.type,
+        repNationCd: selected.country,
+      });
     } catch (error) {
       showBoundary(error);
     }
@@ -48,7 +46,9 @@ function useMovieData({ selected }: { selected: Record<string, string> }) {
 
   useEffect(() => {
     if (selected.country && selected.type && selected.year) {
-      const promise = getMovieList();
+      const fetchData =
+        process.env.NODE_ENV === 'test' ? getTestMovieList : getMovieList;
+      const promise = fetchData();
       setMovieList(promiseWrapper(promise));
     }
   }, [selected.country, selected.type, selected.year]);
