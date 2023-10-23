3fc4f96ad7b92de2f26f9ca71452289fa9c3203d
Moondoyeon <mmmdo21@gmail.com>
2023-10-23 18:20:21 +0900

    style(loading UI): App.tsx 의 Suspense 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
-import { Suspense, lazy } from 'react';
+import { lazy } from 'react';
 import { URL } from 'constants/url';
 import PageLayout from 'components/@layout/PageLayout';
 import useCatch404Error from 'hooks/useCatch404Error';
 
 const MovieGame = lazy(() => import('pages/MovieGame'));
-const Loading = lazy(() => import('components/@common/Loading'));
 
 function App() {
   useCatch404Error();
   return (
     <PageLayout>
-      <Suspense fallback={<Loading whiteBoard={false} />}>
-        <Routes>
-          <Route path={URL.HOME} element={<MovieGame />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path={URL.HOME} element={<MovieGame />} />
+      </Routes>
     </PageLayout>
   );
 }
