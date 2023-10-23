d3a624cf1d9c607fce85d73c486a3ae5a1208a1e
Moondoyeon <mmmdo21@gmail.com>
2023-10-23 23:22:42 +0900

    chore(font): subset 폰트 사용

--- src/style/reset.ts
+++ src/style/reset.ts
@@ -8,37 +8,29 @@ const reset = css`
     margin: 0;
     padding: 0;
   }
+
   @font-face {
-    font-family: Galmuri11;
-    font-style: normal;
-    font-stretch: normal;
-    font-weight: 400;
-    font-display: swap;
+    font-family: 'Galmuri11';
     src:
       local('Galmuri11 Regular'),
-      url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/Galmuri11.woff2')
-        format('woff2'),
-      url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/Galmuri11.woff')
-        format('woff'),
-      url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/Galmuri11.ttf')
-        format('truetype');
+      url('subset-Galmuri11-Regular.woff2') format('woff2'),
+      url('subset-Galmuri11-Regular.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
   }
 
   @font-face {
-    font-family: Galmuri11;
-    font-style: normal;
-    font-stretch: normal;
-    font-weight: 700;
-    font-display: swap;
+    font-family: 'Galmuri11';
     src:
       local('Galmuri11 Bold'),
-      url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/Galmuri11-Bold.woff2')
-        format('woff2'),
-      url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/Galmuri11-Bold.woff')
-        format('woff'),
-      url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/Galmuri11-Bold.ttf')
-        format('truetype');
+      url('subset-Galmuri11-Bold.woff2') format('woff2'),
+      url('subset-Galmuri11-Bold.woff') format('woff');
+    font-weight: bold;
+    font-style: normal;
+    font-display: swap;
   }
+
   body {
     font-family: 'Noto Sans KR', sans-serif;
     min-width: 320px; // 해당 너비 이하일때, 디자인이 깨지는 것 방지
